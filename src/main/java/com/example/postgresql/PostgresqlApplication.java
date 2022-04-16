e959bc120bb65a6e63ab59a19faff52ef230d308
陳君豪 <justin.uob@gmail.com>
2022-04-16 16:28:41 +0800

    StudentRepository OK

--- src/main/java/com/example/postgresql/PostgresqlApplication.java
+++ src/main/java/com/example/postgresql/PostgresqlApplication.java
@@ -1,13 +1,66 @@
 package com.example.postgresql;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class PostgresqlApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PostgresqlApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+
+        return args -> {
+            studentRepository.deleteAll();
+
+            Student maria = new Student(
+                    "Maria",
+                    "Jones",
+                    "maria.jones@amigoscode.edu",
+                    21
+            );
+            Student ahmed = new Student(
+                    "Ahmed",
+                    "Ali",
+                    "ahmed.ali@amigoscode.edu",
+                    18
+            );
+//            studentRepository.save(maria);
+            System.out.println("Adding maria and ahmed");
+            studentRepository.saveAll(List.of(maria, ahmed));
+            System.out.print("Number of students: ");
+            System.out.println(studentRepository.count());
+
+            studentRepository
+                    .findById(2L)
+                    .ifPresentOrElse(
+                            System.out::println,
+                            () -> System.out.println("Student with ID 2 not found"));
+
+            studentRepository
+                    .findById(3L)
+                    .ifPresentOrElse(
+                            System.out::println,
+                            () -> System.out.println("Student with ID 3 not found"));
+
+            System.out.println("Select all students");
+            List<Student> students = studentRepository.findAll();
+            students.forEach(System.out::println);
+
+//            System.out.println("Delete maria");
+//            studentRepository.deleteById(1L);
+
+            System.out.print("Number of students: ");
+            System.out.println(studentRepository.count());
+        };
+
     }
 
 }
